673a6b3b272d5457aa0d1fc324df3d745c480a09
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2021-11-18 20:57:22 +0200

    react def st-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import MyInfo from "./components/MyInfo/MyInfo";
+import {Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyInfo />
+      <Button variant="primary">Button</Button>
     </div>
   );
 }
---
+++ src/components/MyInfo/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+      <div>
+        <h1>Denys Lutsenko</h1>
+        <p>Student Xai 518st</p>
+        <ul>
+          <li>any place that’s </li>
+          <li>free and </li>
+          <li>delicious</li>
+        </ul>
+      </div>
+    )
+}
+
+export default MyInfo
